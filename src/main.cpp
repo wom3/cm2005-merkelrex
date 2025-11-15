400676e713e185b7e4c3d32652bc38821f02b27a
wom3 <wom2@student.london.ac.uk>
2025-11-15 22:55:49 +0500

    feat: compiled the `main.cpp` and `OrderBookEntry.cpp` after including the header

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,16 @@
 #include <string>
 #include <vector>
 #include <ostream>
+#include "OrderBookEntry.h"
 
-// std::ostream& operator<<(std::ostream& os, const OrderBookType& t) {
-//   switch (t) {
-//   case OrderBookType::ask: os << "ask"; break;
-//   case OrderBookType::bid: os << "bid"; break;
-//   default: os << "unknown"; break;
-//   }
-//   return os;
-// }
+std::ostream& operator<<(std::ostream& os, const OrderBookType& t) {
+  switch (t) {
+  case OrderBookType::ask: os << "ask"; break;
+  case OrderBookType::bid: os << "bid"; break;
+  default: os << "unknown"; break;
+  }
+  return os;
+}
 
 void printMenu()
 {
@@ -144,7 +145,7 @@ int main()
   // }
   // return 0;
 
-  // std::vector<OrderBookEntry> orders;
+  std::vector<OrderBookEntry> orders;
 
   // order1.price = 10000;
   // order1.amount = 0.002;
@@ -152,9 +153,9 @@ int main()
   // order1.product = "BTC/USDT";
   // order1.orderType = OrderBookType::ask;
 
-  // orders.push_back(OrderBookEntry{ 10000, 0.02, "2020/03/18 16:01:24.884492", "BTC/USDT", OrderBookType::bid });
-  // orders.push_back(OrderBookEntry{ 20000, 0.030, "2020/03/19 17:02:24.884492", "BTC/USDT", OrderBookType::ask });
-  // orders.push_back(OrderBookEntry{ 30000, 0.040, "2020/03/20 18:03:24.884492", "BTC/USDT", OrderBookType::bid });
+  orders.push_back(OrderBookEntry{ 10000, 0.02, "2020/03/18 16:01:24.884492", "BTC/USDT", OrderBookType::bid });
+  orders.push_back(OrderBookEntry{ 20000, 0.030, "2020/03/19 17:02:24.884492", "BTC/USDT", OrderBookType::ask });
+  orders.push_back(OrderBookEntry{ 30000, 0.040, "2020/03/20 18:03:24.884492", "BTC/USDT", OrderBookType::bid });
 
 
   // // 1st method of iterating over a vector
@@ -178,14 +179,14 @@ int main()
   // }
 
   // 3rd method of iterating over a vector: using an object style syntax
-  // for (unsigned int i = 0; i < orders.size(); ++i)
-  // {
-  //   std::cout << "The price of order no " << i + 1 << " is " << orders.at(i).price << std::endl;
-  //   std::cout << "The amount of order no " << i + 1 << " is " << orders.at(i).amount << std::endl;
-  //   std::cout << "The timestamp of order no " << i + 1 << " is " << orders.at(i).timestamp << std::endl;
-  //   std::cout << "The product of order no " << i + 1 << " is " << orders.at(i).product << std::endl;
-  //   // std::cout << "The order type of order no " << i + 1 << " is " << orders.at(i).orderType << std::endl;
-  // }
+  for (unsigned int i = 0; i < orders.size(); ++i)
+  {
+    std::cout << "The price of order no " << i + 1 << " is " << orders.at(i).price << std::endl;
+    std::cout << "The amount of order no " << i + 1 << " is " << orders.at(i).amount << std::endl;
+    std::cout << "The timestamp of order no " << i + 1 << " is " << orders.at(i).timestamp << std::endl;
+    std::cout << "The product of order no " << i + 1 << " is " << orders.at(i).product << std::endl;
+    std::cout << "The order type of order no " << i + 1 << " is " << orders.at(i).orderType << std::endl;
+  }
 
 
 }
