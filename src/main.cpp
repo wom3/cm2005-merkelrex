6e986438efb0bb10c9e5f81231209d68a5d6730e
wom3 <wom2@student.london.ac.uk>
2025-11-15 14:20:48 +0500

    feat: represented multiple order book entries via `vector`

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 void printMenu()
 {
@@ -93,23 +94,44 @@ void processUserOption(int userOption)
 
 int main()
 {
-
-  double price = 5319.450228;
-  double amount = 0.00020075;
-
-  std::string timestamp{ "2020/03/17 17:01:24.884492" };
-  std::string product{ "BTC/USDT" };
-  // std::string orderType{ "bid" };
-
   enum class OrderBookType { bid, ask };
-  OrderBookType orderType = OrderBookType::bid;
 
-  while (true)
-  {
-    printMenu();
-    int userOption = getUserOption();
-    processUserOption(userOption);
-  }
+  // double price = 5319.450228;
+  // double amount = 0.00020075;
+  // std::string timestamp{ "2020/03/17 17:01:24.884492" };
+  // std::string product{ "BTC/USDT" };
+  // std::string orderType{ "bid" };
+  // OrderBookType orderType = OrderBookType::bid;
+
+  std::vector<double> prices;
+  std::vector<double> amounts;
+  std::vector<std::string> timestamps;
+  std::vector<std::string> products;
+  std::vector<OrderBookType> orderTypes;
+
+  prices.push_back(5319.450228);
+  amounts.push_back(0.00020075);
+  timestamps.push_back("2020/03/17 17:01:24.884492");
+  products.push_back("BTC/USDT");
+  orderTypes.push_back(OrderBookType::bid);
+
+  prices.push_back(5320.450228);
+  amounts.push_back(0.00020175);
+  timestamps.push_back("2020/03/18 16:01:24.884492");
+  products.push_back("BTC/USDT");
+  orderTypes.push_back(OrderBookType::ask);
+
+  std::cout << "prices: " << prices[0] << std::endl;
+  std::cout << "amounts: " << amounts[0] << std::endl;
+  std::cout << "prices: " << prices[1] << std::endl;
+  std::cout << "amounts: " << amounts[1] << std::endl;
+
+  // while (true)
+  // {
+  //   printMenu();
+  //   int userOption = getUserOption();
+  //   processUserOption(userOption);
+  // }
   return 0;
 }
 
